fc1a578d51983757589875c80d0b15d0dd554aec
devin <devin.holloway@coopersurgical.com>
2024-11-01 18:47:56 -0600

    Add ESLint module and related configuration
    Configured ESLint with a stylistic rule set and added new npm scripts for linting and fixing code. Updated package.json and package-lock.json to include the @nuxt/eslint module and its dependencies.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/eslint'],
+  devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
