0e5b7889ba41dac923bfd2da51f88add4dc62185
devin <devin.holloway@coopersurgical.com>
2024-11-01 18:52:31 -0600

    Add Pinia support to Nuxt configuration
    Included @pinia/nuxt and pinia dependencies to facilitate state management using Pinia in the Nuxt project. Updated nuxt.config.ts and package files accordingly, and made necessary adjustments to ensure compatibility.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@pinia/nuxt'],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
 
