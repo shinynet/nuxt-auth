8cf49912fb1f5bd5c524e4bd131fce60e628783c
Devin <devin@shinynet.com>
2024-11-11 16:25:25 -0700

    Refactor StoreToolbar layout
    Moved search input into toolbar title and added new buttons for filtering by highest rated and low inventory products. These changes improve the overall toolbar functionality and user accessibility.

--- src/components/StoreToolbar.vue
+++ src/components/StoreToolbar.vue
@@ -7,27 +7,45 @@
       round
       @click="$emit('toggle-drawer')"
     />
-    <q-toolbar-title />
-    <q-input
-      v-model="search"
-      borderless
-      class="fullwidth"
-      clearable
-      dark
-      dense
-      filled
-      label="Search Store"
-      square
-    >
-      <template #after>
-        <q-btn
-          dense
-          flat
-          icon="search"
-          padding="24px 4px"
-        />
-      </template>
-    </q-input>
+    <q-toolbar-title>
+      <q-input
+        v-model="search"
+        borderless
+        class="fullwidth"
+        clearable
+        dark
+        dense
+        filled
+        label="Search Store"
+        square
+      >
+        <template #after>
+          <q-btn
+            dense
+            flat
+            icon="search"
+            padding="24px 4px"
+          />
+        </template>
+      </q-input>
+    </q-toolbar-title>
+    <q-btn
+      class="gt-xs"
+      flat
+      icon="star"
+      label="Highest Rated"
+    />
+    <q-btn
+      class="gt-xs"
+      flat
+      icon="inventory"
+      label="Low Inventory"
+    />
+    <q-btn
+      flat
+      icon="shopping_cart"
+      round
+    />
   </q-toolbar>
 </template>
 
